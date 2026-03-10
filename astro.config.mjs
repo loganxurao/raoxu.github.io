a0b624693eda91fd2403b82d8e539b05aa67b813
坦坦 <tantan@tantandeMac-mini.local>
2026-03-11 00:29:46 +0800

    Fix navigation by switching to directory routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://raoxu.com',
+  trailingSlash: 'always',
   build: {
-    format: 'file'
+    format: 'directory'
   }
 });
